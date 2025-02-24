98417cd437abcf624a582c56e3f4ed37a75d25a5
Bedikq <108838382+Bojdans@users.noreply.github.com>
2025-02-25 01:49:51 +0300

    пока воркаем

---
+++ src/main/java/org/example/shooltest/ShooltestApplication.java
@@ -0,0 +1,13 @@
+package org.example.shooltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooltestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShooltestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/shooltest/ShooltestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.shooltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShooltestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
